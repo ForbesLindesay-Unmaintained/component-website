5674728ee6a4332cd4cb278534c2c46189ac103e
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-12-03 19:31:38 +0000

    Re order routes

--- server.js
+++ server.js
@@ -33,10 +33,13 @@ app.configure('production', function(){
 app.get('/', require('./routes/index'));
 app.get('/component-badge.svg', require('./routes/component-badge'));
 
+//static redirects
+app.redirect('/refer/:repo/:user', '/:repo/:user'); // should be 301, but may change in the future
+app.redirect('/:user/:repo/component-badge.svg', '/component-badge.svg');
+
 //dynamic routes
 app.get('/:user', require('./routes/user'));
 app.get('/:user/:repo', require('./routes/repo'));
-app.redirect('/refer/:repo/:user', '/:repo/:user'); // should be 301, but may change in the future
 
 app.get('/:user/:repo/download', require('./routes/download-listing'));
 
@@ -44,8 +47,6 @@ app.get('/:user/:repo/download/latest.js', require('./routes/latest')(false));
 app.get('/:user/:repo/download/latest.min.js', require('./routes/latest')(true));
 
 app.get('/:user/:repo/download/:file.js', require('./routes/download-standalone'));
-
-app.redirect('/:user/:repo/component-badge.svg', '/component-badge.svg');
 
 
 http.createServer(app).listen(app.get('port'), function(){
