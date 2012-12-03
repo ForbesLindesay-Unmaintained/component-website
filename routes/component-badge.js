2558b1b0d9c098b6b48a5b173be46fa9910d366b
Jan Buschtöns <buschtoens@gmail.com>
2012-12-03 17:55:36 +0100

    Scale the logo down to 240 x 60

--- routes/component-badge.js
+++ routes/component-badge.js
@@ -5,5 +5,5 @@ var badge = require("component-badge");
 module.exports = route;
 function route(req, res, next) {
   res.type('svg');
-  res.send(badge(10));
+  res.send(badge(10), { scale: 0.5 });
 }
