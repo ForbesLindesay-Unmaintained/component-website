bf6d052d65404a0bfd55871deab152c74d9f60bd
Jan Buschtöns <buschtoens@gmail.com>
2012-12-03 17:57:03 +0100

    change to /:user/:repo

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/:user/:repo/download/latest.min.js', require('./routes/latest')(true))
 
 app.get('/:user/:repo/download/:file.js', require('./routes/download-standalone'));
 
-app.redirect('/:repo/:user/component-badge.svg', '/component-badge.svg');
+app.redirect('/:user/:repo/component-badge.svg', '/component-badge.svg');
 app.get('/component-badge.svg', require('./routes/component-badge'));
 
 
